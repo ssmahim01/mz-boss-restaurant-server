56ec66905f4d91a355b9aaa8971799d436594566
ssmahim01 <mahimzayn666@gmail.com>
2025-01-02 18:24:21 +0600

    Added crucial files for server works

---
+++ index.js
@@ -0,0 +1,81 @@
+const express = require('express');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ybs8l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const menuCollection = client.db("mz_bossDB").collection("menu");
+    const reviewCollection = client.db("mz_bossDB").collection("reviews");
+    const cartCollection = client.db("mz_bossDB").collection("carts");
+
+    app.get("/menu", async(req, res) => {
+      const result = await menuCollection.find().toArray();
+      res.send(result);
+    });
+    
+    app.get("/reviews", async(req, res) => {
+      const result = await reviewCollection.find().toArray();
+      res.send(result);
+    });
+
+    // Cart collection
+    app.get("/carts", async(req, res) => {
+      const email = req.query.email;
+      const query = {email: email};
+      const getResult = await cartCollection.find(query).toArray();
+      res.send(getResult);
+    });
+
+    app.post("/carts", async(req, res) => {
+      const cartItem = req.body;
+      const result = await cartCollection.insertOne(cartItem);
+      res.send(result);
+    });
+
+    app.delete("/cart/:id", async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Mahim Boss is Sitting');
+});
+
+app.listen(port, () => {
+    console.log(`MZ Boss is sitting on port ${port}`);
+});
